f4d7c9544130ebb33365cca60e3c1e792fa10071
pkro <pkrombach@web.de>
2020-08-04 12:40:02 +0200

    before 2.4

--- main.js
+++ main.js
@@ -1,63 +1,42 @@
-func = new $f();
-func.hello();
-// Game
-//   Info section
-//   Deck
-//   Discard Pile
-//   Rule
-
-// Deck
-//   Cards
-//   ----
-//   shuffle
-//   stack
-
-// Card
-//   val
-//   suit
-//   ----
-//   flip
-
-// Discard Pile
-//   Holders
-//   ----
-//   accept or reject
+'use strict';
 
 // iife to avoid polution of global namespace
 (function (window) {
   // Game
   const Game = function (el, option) {
     this.el = document.getElementById(el);
-
     this.option = option;
-    //   Info section
-    this.info_div = createDiv('info_div');
+    // 	Info section
 
-    this.deck_div = createDiv('deck_div');
+    this.info_div = document.createElement('div');
+    this.info_div.id = 'info_div';
+    // 	Deck
+
+    this.deck_div = document.createElement('div');
+    this.deck_div.id = 'deck_div';
+
     this.gameDeck = new Deck(this.deck_div, option);
     this.gameDeck.buildDeck();
+    // 	Discard Pile
+    // 	Rules
 
-    // Discard pile
-    // Rules
-    this.fragment = document.createDocumentFragment();
-    this.fragment.appendChild(this.info_div);
-    this.fragment.appendChild(this.info_div);
-    this.el.appendChild(this.fragment);
+    this.el.appendChild(this.info_div);
+    this.el.appendChild(this.deck_div);
   };
 
   // Deck
   const Deck = function (deck_div, option) {
     this.deckData = option.data;
     this.buildDeck = function () {
-      var fragment = document.createDocumentFragment();
+      var parentFrag = document.createDocumentFragment();
       deck_div.innerHTML = '';
       for (var i = this.deckData.length - 1; i >= 0; i--) {
         var card = new Card();
         card.id = 'card-' + i;
         card.data = this.deckData[i];
-        card.buildCard(fragment);
+        card.buildCard(parentFrag);
       }
-      deck_div.appendChild(fragment);
+      deck_div.appendChild(parentFrag);
     };
 
     // Cards
@@ -67,18 +46,44 @@ func.hello();
   };
 
   // Card
-  const Card = function (val, suit) {
+  const Card = function () {
     this.id = '';
     this.data = '';
-    this.cardCont = createDiv('card_container');
-    this.cardFront = createDiv('card_front');
     this.cardBack = document.createElement('div');
     this.cardBack.className = 'card_back';
-    this.buildCard = function () {
-      const flipDiv = createDiv('flip'),
-        frontValDiv = createDiv('front_val'),
-        backValDiv = createDiv('back_val'),
-        catDiv = createDiv('cat_val');
+    this.cardCont = document.createElement('div');
+    this.cardCont.className = 'card_container';
+    this.cardFront = document.createElement('div');
+    this.cardFront.className = 'card_front';
+
+    this.buildCard = function (parentFrag) {
+      const flipDiv = document.createElement('div'),
+        frontValDiv = document.createElement('div'),
+        backValDiv = document.createElement('div'),
+        catDiv = document.createElement('div');
+
+      flipDiv.className = 'flip';
+      frontValDiv.className = 'front_val';
+      backValDiv.className = 'back_val';
+      catDiv.className = 'cat_val';
+
+      frontValDiv.innerHTML = this.data.q;
+      backValDiv.innerHTML = this.data.a;
+      catDiv.innerHTML = this.data.category;
+      this.cardFront.appendChild(frontValDiv);
+      this.cardFront.appendChild(catDiv);
+      this.cardBack.appendChild(backValDiv);
+      flipDiv.appendChild(this.cardFront);
+      flipDiv.appendChild(this.cardBack);
+
+      this.cardCont.id = this.id;
+      this.cardCont.appendChild(flipDiv);
+
+      this.cardCont.onclick = function (e) {
+        e.currentTarget.classList.toggle('flip_card');
+        e.currentTarget.classList.toggle('slide_over');
+      };
+      parentFrag.appendChild(this.cardCont);
     };
 
     //   val
--- test.js
+++
@@ -1,8 +0,0 @@
-(function (window) {
-  function $f() {}
-
-  $f.prototype.hello = function () {
-    console.log('hello');
-  };
-  window.$f = $f;
-})(window);
